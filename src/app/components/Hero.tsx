c3401e2916e18d8fa44e02a6e1f319c797abf4cb
unknown <yuldann6@gmail.com>
2024-10-27 08:53:44 +0700

    line

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,8 +2,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import anime from "animejs/lib/anime.es.js";
+import { BackgroundLines } from "@/components/ui/background-lines";
 
-const Hero = () => {
+
+export function Hero() {
   const imageRef = useRef<HTMLDivElement>(null);
   const textRefs = useRef<HTMLHeadingElement[]>([]);
   const buttonRef = useRef<HTMLButtonElement | null>(null); // Use single ref for the button
@@ -61,8 +63,6 @@ const Hero = () => {
       targets: buttonRef.current,
       scale: [0.8, 1],
       duration: 500,
-
-      
     });
 
     // Scroll to the "Product" section after the animation
@@ -91,51 +91,54 @@ const Hero = () => {
       easing: "easeInOutSine",
     });
   };
-
   return (
-    <div
-      className="h-[100%] w-[100%] flex justify-center items-center pt-[4.2rem] relative -top-[4.2rem]"
-      id="Hero"
-    >
-      <div className="h-[90vh] w-[90vw] rounded-3xl shadow-2xl flex text-black">
-        <div className="w-[100%] h-[100%] justify-center text-center flex items-center p-10 text-2xl">
-          <div>
-            <h1 className="nunito text-5xl lg:text-[4rem]" ref={addToTextRefs}>
-              {displayedText.split("Astrophile")[0]}
-              <span className="span">
-                {displayedText.includes("Astrophile") && "Astrophile"}
-              </span>
-            </h1>
-            <h5
-              className="mt-8 font-sans font-semibold text-[.75rem] sm:text-[1rem] md:text-[1.2rem]"
-              ref={addToTextRefs}
-            >
-              Starry Accessories for Starry Minds Shop Now at Astrophile!
-            </h5>
-            <button
-              className="bg-[#002d64] p-2 rounded-[10px] text-sm nunito pl-5 pr-5 mt-[3rem] hover:bg-[#3f80cf] hover:text-white transition-all duration-500 text-white"
-              ref={buttonRef} // Use single ref for button
-              onClick={handleButtonClick} // Handle button click
-              onMouseDown={handleMouseDown} // Handle mouse down event
-              onMouseUp={handleMouseUp} // Handle mouse up event
-              onMouseLeave={handleMouseUp} // Ensure scaling back if mouse leaves
-            >
-              Beli Sekarang
-            </button>
+    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 bg-black h-full pb-10 relative -top-[5rem] rounded-b-3xl">
+      <div
+        className="h-[100%] w-[100%] flex justify-center items-center pt-[4.2rem] relative -top-[4.2rem]"
+        id="Hero"
+      >
+        <div className="h-[90vh] w-[90vw] rounded-3xl shadow-2xl flex text-white">
+          <div className="w-[100%] h-[100%] justify-center text-center flex items-center p-10 text-2xl ">
+            <div>
+              <h1
+                className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight"
+                ref={addToTextRefs}
+              >
+                {displayedText.split("Astrophile")[0]}
+                <span className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+                  {displayedText.includes("Astrophile") && "Astrophile"}
+                </span>
+              </h1>
+              <h5
+                className="mt-8 font-sans font-semibold text-[.75rem] sm:text-[1rem] md:text-[1.2rem]"
+                ref={addToTextRefs}
+              >
+                Starry Accessories for Starry Minds Shop Now at Astrophile!
+              </h5>
+              <button
+                className="bg-[#002d64] p-2 rounded-[10px] text-sm nunito pl-5 pr-5 mt-[3rem] hover:bg-[#3f80cf] hover:text-white transition-all duration-500 text-white"
+                ref={buttonRef} // Use single ref for button
+                onClick={handleButtonClick} // Handle button click
+                onMouseDown={handleMouseDown} // Handle mouse down event
+                onMouseUp={handleMouseUp} // Handle mouse up event
+                onMouseLeave={handleMouseUp} // Ensure scaling back if mouse leaves
+              >
+                Beli Sekarang
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center relative" ref={imageRef}>
+            <Image
+              src="/logo1.png"
+              width={300}
+              height={300}
+              alt="logo"
+              className="hidden md:flex rounded-[50%] aspect-square object-cover relative right-10"
+            />
           </div>
         </div>
-        <div className="flex items-center relative" ref={imageRef}>
-          <Image
-            src="/logo1.png"
-            width={300}
-            height={300}
-            alt="logo"
-            className="hidden md:flex rounded-[50%] aspect-square object-cover relative right-10"
-          />
-        </div>
       </div>
-    </div>
+    </BackgroundLines>
   );
-};
-
+}
 export default Hero;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,16 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 
 const Login: React.FC = () => {
   const [formData, setFormData] = useState({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
   });
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const router = useRouter();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -21,7 +22,7 @@ const Login: React.FC = () => {
 
     // Reset error on change
     if (error) {
-      setError('');
+      setError("");
     }
   };
 
@@ -29,94 +30,118 @@ const Login: React.FC = () => {
     e.preventDefault();
 
     // Validate input
-    if (formData.username === '' || formData.password === '') {
-      setError('Please fill in all fields.');
+    if (formData.username === "" || formData.password === "") {
+      setError("Please fill in all fields.");
       return;
     }
 
     try {
       // Send login request to the API
-      const response = await fetch('https://8181-202-46-68-26.ngrok-free.app/api/v1/auth/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: formData.username,
-          password: formData.password,
-        }),
-      });
+      const response = await fetch(
+        "https://8181-202-46-68-26.ngrok-free.app/api/v1/auth/signin",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: formData.username,
+            password: formData.password,
+          }),
+        }
+      );
 
       if (response.status === 401) {
         // Set custom message for unauthorized access
-        setError('Email atau kata sandi salah.'); // Custom message for incorrect login
+        setError("Email atau kata sandi salah."); // Custom message for incorrect login
         return;
       }
 
       if (!response.ok) {
         // Handle other error responses from the API
         const errorData = await response.json();
-        setError(errorData.message || 'Login failed!');
+        setError(errorData.message || "Login failed!");
         return;
       }
 
       // Optionally, handle successful login (e.g., save token, user data)
       const data = await response.json();
-      console.log('Login successful:', data);
+      console.log("Login successful:", data);
 
       // Redirect to the home page after successful login
-      router.push('/');
+      router.push("/");
     } catch (err) {
-      setError('An error occurred while logging in. Please try again.');
-      console.error('Login error:', err);
+      setError("An error occurred while logging in. Please try again.");
+      console.error("Login error:", err);
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-6 rounded-lg shadow-md w-full max-w-md"
+    <div className="block md:flex justify-between items-center h-full md:h-screen bg-gray-100">
+      <div
+        id="left"
+        className="w-full md:w-1/2 h-full flex items-center justify-center pt-10 pb-10 bg-white"
       >
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleChange}
-          className="mb-4 w-full p-2 border rounded"
-          required
-        />
-
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          className="mb-4 w-full p-2 border rounded"
-          required
-        />
-
-        {error && <p className="text-sm text-red-600 mt-1">{error}</p>}
-
-        <button
-          type="submit"
-          className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700 transition-colors"
+        <div className="text-center">
+          <Image
+            src="/logo1.png"
+            width={200}
+            height={200}
+            alt="logo"
+            className="rounded-full aspect-square object-cover"
+          />
+          <h3 className="nunito mt-5 mb-10">
+            ASTRO<span className="span">PHILE</span>
+          </h3>
+        </div>
+      </div>
+      <div id="right" className="md:w-1/2 md:flex md:justify-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white p-6 rounded-lg shadow-md w-full max-w-md"
         >
-          Login
-        </button>
+          <h2 className="text-2xl font-bold mb-6 text-center nunito">LOGIN</h2>
 
-        {/* Add the register link here */}
-        <p className="mt-4 text-sm text-center">
-          Belum punya akun?{" "}
-          <Link href="/register" className="text-blue-600 hover:underline">
-            Register
-          </Link>
-        </p>
-      </form>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
+            className="mb-4 w-full p-2 border rounded"
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            className="mb-4 w-full p-2 border rounded"
+            required
+          />
+
+          {error && <p className="text-sm text-red-600 mt-1">{error}</p>}
+
+          <div id="button" className="w-full flex justify-center mt-5">
+            <button
+              type="submit"
+              className="w-1/4 bg-blue-600 text-white p-2 rounded hover:bg-blue-700 transition-colors"
+            >
+              Login
+            </button>
+          </div>
+
+          {/* Add the register link here */}
+          <p className="mt-4 text-sm text-center">
+            Belum punya akun?{" "}
+            <Link href="/register" className="text-blue-600 hover:underline">
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
